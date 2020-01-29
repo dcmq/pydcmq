0f588dbfecc0ee3453c708c894b04475ae80b8a6
vsaase <vsaase@gmail.com>
2020-01-29 16:18:10 +0100

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pydcmq", # Replace with your own username
+    name="pydcmq",
     version="0.0.1",
     author="Victor Saase",
     author_email="vsaase@gmail.com",
